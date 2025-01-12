f128402064d625b3ad80aadc8d9c224b5313959f
Dnpyn2g <100103799+Dnpyn2g@users.noreply.github.com>
2025-01-12 13:09:59 +0200

    уц
    впв

--- js/ads.js
+++
@@ -1,8 +0,0 @@
-// Google AdSense
-(function() {
-    const script = document.createElement('script');
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4048193958934146";
-    script.async = true;
-    script.setAttribute("crossorigin", "anonymous");
-    document.head.appendChild(script);
-})();

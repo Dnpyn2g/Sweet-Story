d5159ec2496d971115186e99743cf87989ff774f
Dnpyn2g <100103799+Dnpyn2g@users.noreply.github.com>
2025-04-11 22:28:11 +0300

    ыыы

--- js/gallery.js
+++ js/gallery.js
@@ -1,87 +1,109 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Пример: создаём массив с данными историй.
-    // В реальном проекте данные можно получать через AJAX или fetch.
-    var stories = [];
-    for (var i = 0; i < 20; i++) {
-        stories.push({
-            title: "История " + (i + 1),
-            content: "Содержимое истории номер " + (i + 1) + "..."
-        });
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('stories.json')
+        .then(response => response.json())
+        .then(stories => {
+            const gallery = document.getElementById('story-gallery');
 
-    var currentPage = 1;
-    var storiesPerPage = 5; // Количество историй на одной странице
-    var storyGallery = document.getElementById("story-gallery");
-    var paginationElement = document.getElementById("pagination");
+            // Очистка галереи перед добавлением
+            gallery.innerHTML = '';
 
-    // Функция для отображения историй текущей страницы
-    function displayStories(page) {
-        storyGallery.innerHTML = "";
-        var start = (page - 1) * storiesPerPage;
-        var end = start + storiesPerPage;
-        var pageStories = stories.slice(start, end);
+            // Переворот массива для отображения от новых к старым
+            stories.reverse().forEach(story => {
+                const storyItem = document.createElement('div');
+                storyItem.className = 'story-item';
 
-        pageStories.forEach(function(story) {
-            var article = document.createElement('article');
-            var title = document.createElement('h3');
-            title.innerText = story.title;
-            var content = document.createElement('p');
-            content.innerText = story.content;
-            article.appendChild(title);
-            article.appendChild(content);
-            storyGallery.appendChild(article);
-        });
-        updatePagination();
-    }
+                storyItem.innerHTML = `
+                    <a href="story1.html?id=${story.id}">
+                        <img src="${story.image}" alt="${story.title}">
+                    </a>
+                    <p>${story.title}</p>
+                    <a href="story1.html?id=${story.id}">Читать больше</a>
+                `;
 
-    // Функция для создания и обновления элементов пагинации
-    function updatePagination() {
-        // Очищаем блок пагинации
-        paginationElement.innerHTML = "";
-        var totalPages = Math.ceil(stories.length / storiesPerPage);
+                gallery.appendChild(storyItem);
+            });
 
-        // Кнопка "Предыдущая"
-        var prevButton = document.createElement('button');
-        prevButton.innerText = "Предыдущая";
-        prevButton.disabled = currentPage === 1;
-        prevButton.addEventListener('click', function() {
-            if (currentPage > 1) {
-                currentPage--;
-                displayStories(currentPage);
+            // Добавление первой рекламы в случайное место
+            const adContainer1 = document.createElement('div');
+            adContainer1.id = 'bn_60d96ced6b';
+
+            const randomIndex1 = Math.floor(Math.random() * (stories.length + 1));
+            const storyItems1 = Array.from(gallery.children);
+
+            if (randomIndex1 < storyItems1.length) {
+                gallery.insertBefore(adContainer1, storyItems1[randomIndex1]);
+            } else {
+                gallery.appendChild(adContainer1);
             }
-        });
-        paginationElement.appendChild(prevButton);
 
-        // Кнопки с номерами страниц
-        for (var i = 1; i <= totalPages; i++) {
-            var pageButton = document.createElement('button');
-            pageButton.innerText = i;
-            if (i === currentPage) {
-                pageButton.classList.add('active');
+            // Подключение первого рекламного скрипта
+            const adScript1 = document.createElement('script');
+            adScript1.type = 'text/javascript';
+            adScript1.textContent = `
+                'use strict';(function(C,b,m,r){function t(){b.removeEventListener("scroll",t);f()}function u(){p=new IntersectionObserver(a=>{a.forEach(n=>{n.isIntersecting&&(p.unobserve(n.target),f())})},{root:null,rootMargin:"400px 200px",threshold:0});p.observe(e)}function f(){(e=e||b.getElementById("bn_"+m))?(e.innerHTML="",e.id="bn_"+v,q={act:"init",id:m,rnd:v,ms:w},(d=b.getElementById("rcMain"))?c=d.contentWindow:D(),c.rcMain?c.postMessage(q,x):c.rcBuf.push(q)):g("!bn")}function E(a,n,F,y){function z(){var h=
+                n.createElement("script");h.type="text/javascript";h.src=a;h.onerror=function(){k++;5>k?setTimeout(z,10):g(k+"!"+a)};h.onload=function(){y&&y();k&&g(k+"!"+a)};F.appendChild(h)}var k=0;z()}function D(){try{d=b.createElement("iframe"),d.style.setProperty("display","none","important"),d.id="rcMain",b.body.insertBefore(d,b.body.children[0]),c=d.contentWindow,l=c.document,l.open(),l.close(),A=l.body,Object.defineProperty(c,"rcBuf",{enumerable:!1,configurable:!1,writable:!1,value:[]}),E("https://go.rcvlink.com/static/main.js",
+                l,A,function(){for(var a;c.rcBuf&&(a=c.rcBuf.shift());)c.postMessage(a,x)})}catch(a){B(a)}}function B(a){g(a.name+": "+a.message+"\t"+(a.stack?a.stack.replace(a.name+": "+a.message,""):""))}function g(a){console.error(a);(new Image).src="https://go.rcvlinks.com/err/?code="+m+"&ms="+((new Date).getTime()-w)+"&ver="+G+"&text="+encodeURIComponent(a)}try{var G="231101-0007",x=location.origin||location.protocol+"//"+location.hostname+(location.port?":"+location.port:""),e=b.getElementById("bn_"+m),v=Math.random().toString(36).substring(2,
+                15),w=(new Date).getTime(),p,H=!("IntersectionObserver"in C),q,d,c,l,A;e?"scroll"==r?b.addEventListener("scroll",t):"lazy"==r?H?f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",u):u():f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",f):g("!bn")}catch(a){B(a)}})(window,document,"60d96ced6b","{LOADTYPE}");
+            `;
+            document.body.appendChild(adScript1);
+
+            // Добавление второй рекламы в случайное место
+            const adContainer2 = document.createElement('div');
+            adContainer2.id = 'bn_a5bd653abc';
+
+            const randomIndex2 = Math.floor(Math.random() * (stories.length + 1));
+            const storyItems2 = Array.from(gallery.children);
+
+            if (randomIndex2 < storyItems2.length) {
+                gallery.insertBefore(adContainer2, storyItems2[randomIndex2]);
+            } else {
+                gallery.appendChild(adContainer2);
             }
-            // Используем IIFE для привязки переменной i
-            (function(page) {
-                pageButton.addEventListener('click', function() {
-                    currentPage = page;
-                    displayStories(currentPage);
+
+            // Подключение второго рекламного скрипта
+            const adScript2 = document.createElement('script');
+            adScript2.type = 'text/javascript';
+            adScript2.textContent = `
+                'use strict';(function(C,b,m,r){function t(){b.removeEventListener("scroll",t);f()}function u(){p=new IntersectionObserver(a=>{a.forEach(n=>{n.isIntersecting&&(p.unobserve(n.target),f())})},{root:null,rootMargin:"400px 200px",threshold:0});p.observe(e)}function f(){(e=e||b.getElementById("bn_"+m))?(e.innerHTML="",e.id="bn_"+v,q={act:"init",id:m,rnd:v,ms:w},(d=b.getElementById("rcMain"))?c=d.contentWindow:D(),c.rcMain?c.postMessage(q,x):c.rcBuf.push(q)):g("!bn")}function E(a,n,F,y){function z(){var h=
+                n.createElement("script");h.type="text/javascript";h.src=a;h.onerror=function(){k++;5>k?setTimeout(z,10):g(k+"!"+a)};h.onload=function(){y&&y();k&&g(k+"!"+a)};F.appendChild(h)}var k=0;z()}function D(){try{d=b.createElement("iframe"),d.style.setProperty("display","none","important"),d.id="rcMain",b.body.insertBefore(d,b.body.children[0]),c=d.contentWindow,l=c.document,l.open(),l.close(),A=l.body,Object.defineProperty(c,"rcBuf",{enumerable:!1,configurable:!1,writable:!1,value:[]}),E("https://go.rcvlink.com/static/main.js",
+                l,A,function(){for(var a;c.rcBuf&&(a=c.rcBuf.shift());)c.postMessage(a,x)})}catch(a){B(a)}}function B(a){g(a.name+": "+a.message+"\t"+(a.stack?a.stack.replace(a.name+": "+a.message,""):""))}function g(a){console.error(a);(new Image).src="https://go.rcvlinks.com/err/?code="+m+"&ms="+((new Date).getTime()-w)+"&ver="+G+"&text="+encodeURIComponent(a)}try{var G="231101-0007",x=location.origin||location.protocol+"//"+location.hostname+(location.port?":"+location.port:""),e=b.getElementById("bn_"+m),v=Math.random().toString(36).substring(2,
+                15),w=(new Date).getTime(),p,H=!("IntersectionObserver"in C),q,d,c,l,A;e?"scroll"==r?b.addEventListener("scroll",t):"lazy"==r?H?f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",u):u():f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",f):g("!bn")}catch(a){B(a)}})(window,document,"a5bd653abc","{LOADTYPE}");
+            `;
+            document.body.appendChild(adScript2);
+
+            // Добавление третьей рекламы в случайное место
+            const adContainer3 = document.createElement('div');
+            adContainer3.id = 'bn_eeeac28f28';
+
+            const randomIndex3 = Math.floor(Math.random() * (stories.length + 1));
+            const storyItems3 = Array.from(gallery.children);
+
+            if (randomIndex3 < storyItems3.length) {
+                gallery.insertBefore(adContainer3, storyItems3[randomIndex3]);
+            } else {
+                gallery.appendChild(adContainer3);
+            }
+
+            // Подключение третьего рекламного скрипта
+            const adScript3 = document.createElement('script');
+            adScript3.type = 'text/javascript';
+            adScript3.textContent = `
+                'use strict';(function(C,b,m,r){function t(){b.removeEventListener("scroll",t);f()}function u(){p=new IntersectionObserver(a=>{a.forEach(n=>{n.isIntersecting&&(p.unobserve(n.target),f())})},{root:null,rootMargin:"400px 200px",threshold:0});p.observe(e)}function f(){(e=e||b.getElementById("bn_"+m))?(e.innerHTML="",e.id="bn_"+v,q={act:"init",id:m,rnd:v,ms:w},(d=b.getElementById("rcMain"))?c=d.contentWindow:D(),c.rcMain?c.postMessage(q,x):c.rcBuf.push(q)):g("!bn")}function E(a,n,F,y){function z(){var h=
+                n.createElement("script");h.type="text/javascript";h.src=a;h.onerror=function(){k++;5>k?setTimeout(z,10):g(k+"!"+a)};h.onload=function(){y&&y();k&&g(k+"!"+a)};F.appendChild(h)}var k=0;z()}function D(){try{d=b.createElement("iframe"),d.style.setProperty("display","none","important"),d.id="rcMain",b.body.insertBefore(d,b.body.children[0]),c=d.contentWindow,l=c.document,l.open(),l.close(),A=l.body,Object.defineProperty(c,"rcBuf",{enumerable:!1,configurable:!1,writable:!1,value:[]}),E("https://go.rcvlink.com/static/main.js",
+                l,A,function(){for(var a;c.rcBuf&&(a=c.rcBuf.shift());)c.postMessage(a,x)})}catch(a){B(a)}}function B(a){g(a.name+": "+a.message+"\t"+(a.stack?a.stack.replace(a.name+": "+a.message,""):""))}function g(a){console.error(a);(new Image).src="https://go.rcvlinks.com/err/?code="+m+"&ms="+((new Date).getTime()-w)+"&ver="+G+"&text="+encodeURIComponent(a)}try{var G="231101-0007",x=location.origin||location.protocol+"//"+location.hostname+(location.port?":"+location.port:""),e=b.getElementById("bn_"+m),v=Math.random().toString(36).substring(2,
+                15),w=(new Date).getTime(),p,H=!("IntersectionObserver"in C),q,d,c,l,A;e?"scroll"==r?b.addEventListener("scroll",t):"lazy"==r?H?f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",u):u():f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",f):g("!bn")}catch(a){B(a)}})(window,document,"eeeac28f28","{LOADTYPE}");
+            `;
+            document.body.appendChild(adScript3);
+
+            // Тест: добавление обработчиков событий для проверки кликов
+            document.querySelectorAll('.story-item a').forEach(link => {
+                link.addEventListener('click', (event) => {
+                    console.log('Клик по ссылке:', link.href);
                 });
-            })(i);
-            paginationElement.appendChild(pageButton);
-        }
-
-        // Кнопка "Следующая"
-        var nextButton = document.createElement('button');
-        nextButton.innerText = "Следующая";
-        nextButton.disabled = currentPage === totalPages;
-        nextButton.addEventListener('click', function() {
-            if (currentPage < totalPages) {
-                currentPage++;
-                displayStories(currentPage);
-            }
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка загрузки историй:', error);
+            document.getElementById('story-gallery').innerHTML = '<p>Не удалось загрузить истории. Попробуйте позже.</p>';
         });
-        paginationElement.appendChild(nextButton);
-    }
-
-    // Изначальное отображение первой страницы
-    displayStories(currentPage);
 });

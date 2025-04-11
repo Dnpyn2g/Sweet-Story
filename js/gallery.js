5c6361ba21b4a4310835982f4bd0314f914354cc
Dnpyn2g <100103799+Dnpyn2g@users.noreply.github.com>
2025-04-11 22:03:26 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,110 +2,105 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('stories.json')
         .then(response => response.json())
         .then(stories => {
-            // Переворачиваем массив, чтобы сначала были новые истории
-            const allStories = stories.reverse();
-            const itemsPerPage = 6; // Количество историй на странице
-            let currentPage = 1;
-            const totalPages = Math.ceil(allStories.length / itemsPerPage);
-
             const gallery = document.getElementById('story-gallery');
-            // В HTML желательно создать контейнер для управления пагинацией, например:
-            // <div id="pagination-container"></div>
-            const paginationContainer = document.getElementById('pagination-container');
 
-            // Функция для отрисовки историй для выбранной страницы
-            function renderPage(page) {
-                gallery.innerHTML = ''; // Очистка галереи
+            // Очистка галереи перед добавлением
+            gallery.innerHTML = '';
 
-                // Вычисляем индексы для среза массива
-                const startIndex = (page - 1) * itemsPerPage;
-                const endIndex = startIndex + itemsPerPage;
-                const pageStories = allStories.slice(startIndex, endIndex);
+            // Переворот массива для отображения от новых к старым
+            stories.reverse().forEach(story => {
+                const storyItem = document.createElement('div');
+                storyItem.className = 'story-item';
 
-                // Отрисовываем истории для текущей страницы
-                pageStories.forEach(story => {
-                    const storyItem = document.createElement('div');
-                    storyItem.className = 'story-item';
+                storyItem.innerHTML = `
+                    <a href="story1.html?id=${story.id}">
+                        <img src="${story.image}" alt="${story.title}">
+                    </a>
+                    <p>${story.title}</p>
+                    <a href="story1.html?id=${story.id}">Читать больше</a>
+                `;
 
-                    storyItem.innerHTML = `
-                        <a href="story1.html?id=${story.id}">
-                            <img src="${story.image}" alt="${story.title}">
-                        </a>
-                        <p>${story.title}</p>
-                        <a href="story1.html?id=${story.id}">Читать больше</a>
-                    `;
-                    gallery.appendChild(storyItem);
-                });
+                gallery.appendChild(storyItem);
+            });
 
-                // Если реклама нужна на каждой странице, добавляем её после отрисовки историй.
-                // Пример для первого рекламного блока:
-                addAds();
+            // Добавление первой рекламы в случайное место
+            const adContainer1 = document.createElement('div');
+            adContainer1.id = 'bn_60d96ced6b';
 
-                // Привязка обработчиков событий, если требуется
-                document.querySelectorAll('.story-item a').forEach(link => {
-                    link.addEventListener('click', event => {
-                        console.log('Клик по ссылке:', link.href);
-                    });
-                });
+            const randomIndex1 = Math.floor(Math.random() * (stories.length + 1));
+            const storyItems1 = Array.from(gallery.children);
+
+            if (randomIndex1 < storyItems1.length) {
+                gallery.insertBefore(adContainer1, storyItems1[randomIndex1]);
+            } else {
+                gallery.appendChild(adContainer1);
             }
 
-            // Функция для добавления рекламы (можно адаптировать под все три ваших рекламных скрипта)
-            function addAds() {
-                // Пример добавления первого рекламного блока:
-                const adContainer1 = document.createElement('div');
-                adContainer1.id = 'bn_60d96ced6b';
+            // Подключение первого рекламного скрипта
+            const adScript1 = document.createElement('script');
+            adScript1.type = 'text/javascript';
+            adScript1.textContent = `
+                'use strict';(function(C,b,m,r){function t(){b.removeEventListener("scroll",t);f()}function u(){p=new IntersectionObserver(a=>{a.forEach(n=>{n.isIntersecting&&(p.unobserve(n.target),f())})},{root:null,rootMargin:"400px 200px",threshold:0});p.observe(e)}function f(){(e=e||b.getElementById("bn_"+m))?(e.innerHTML="",e.id="bn_"+v,q={act:"init",id:m,rnd:v,ms:w},(d=b.getElementById("rcMain"))?c=d.contentWindow:D(),c.rcMain?c.postMessage(q,x):c.rcBuf.push(q)):g("!bn")}function E(a,n,F,y){function z(){var h=
+                n.createElement("script");h.type="text/javascript";h.src=a;h.onerror=function(){k++;5>k?setTimeout(z,10):g(k+"!"+a)};h.onload=function(){y&&y();k&&g(k+"!"+a)};F.appendChild(h)}var k=0;z()}function D(){try{d=b.createElement("iframe"),d.style.setProperty("display","none","important"),d.id="rcMain",b.body.insertBefore(d,b.body.children[0]),c=d.contentWindow,l=c.document,l.open(),l.close(),A=l.body,Object.defineProperty(c,"rcBuf",{enumerable:!1,configurable:!1,writable:!1,value:[]}),E("https://go.rcvlink.com/static/main.js",
+                l,A,function(){for(var a;c.rcBuf&&(a=c.rcBuf.shift());)c.postMessage(a,x)})}catch(a){B(a)}}function B(a){g(a.name+": "+a.message+"\t"+(a.stack?a.stack.replace(a.name+": "+a.message,""):""))}function g(a){console.error(a);(new Image).src="https://go.rcvlinks.com/err/?code="+m+"&ms="+((new Date).getTime()-w)+"&ver="+G+"&text="+encodeURIComponent(a)}try{var G="231101-0007",x=location.origin||location.protocol+"//"+location.hostname+(location.port?":"+location.port:""),e=b.getElementById("bn_"+m),v=Math.random().toString(36).substring(2,
+                15),w=(new Date).getTime(),p,H=!("IntersectionObserver"in C),q,d,c,l,A;e?"scroll"==r?b.addEventListener("scroll",t):"lazy"==r?H?f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",u):u():f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",f):g("!bn")}catch(a){B(a)}})(window,document,"60d96ced6b","{LOADTYPE}");
+            `;
+            document.body.appendChild(adScript1);
 
-                const storyItems = Array.from(gallery.children);
-                const randomIndex1 = Math.floor(Math.random() * (storyItems.length + 1));
+            // Добавление второй рекламы в случайное место
+            const adContainer2 = document.createElement('div');
+            adContainer2.id = 'bn_a5bd653abc';
 
-                if (randomIndex1 < storyItems.length) {
-                    gallery.insertBefore(adContainer1, storyItems[randomIndex1]);
-                } else {
-                    gallery.appendChild(adContainer1);
-                }
-                
-                // Если необходимо, вставьте сюда инициализацию рекламного скрипта, аналогично вашему исходному коду.
+            const randomIndex2 = Math.floor(Math.random() * (stories.length + 1));
+            const storyItems2 = Array.from(gallery.children);
+
+            if (randomIndex2 < storyItems2.length) {
+                gallery.insertBefore(adContainer2, storyItems2[randomIndex2]);
+            } else {
+                gallery.appendChild(adContainer2);
             }
 
-            // Функция для создания элементов управления пагинацией
-            function createPaginationControls() {
-                paginationContainer.innerHTML = '';
+            // Подключение второго рекламного скрипта
+            const adScript2 = document.createElement('script');
+            adScript2.type = 'text/javascript';
+            adScript2.textContent = `
+                'use strict';(function(C,b,m,r){function t(){b.removeEventListener("scroll",t);f()}function u(){p=new IntersectionObserver(a=>{a.forEach(n=>{n.isIntersecting&&(p.unobserve(n.target),f())})},{root:null,rootMargin:"400px 200px",threshold:0});p.observe(e)}function f(){(e=e||b.getElementById("bn_"+m))?(e.innerHTML="",e.id="bn_"+v,q={act:"init",id:m,rnd:v,ms:w},(d=b.getElementById("rcMain"))?c=d.contentWindow:D(),c.rcMain?c.postMessage(q,x):c.rcBuf.push(q)):g("!bn")}function E(a,n,F,y){function z(){var h=
+                n.createElement("script");h.type="text/javascript";h.src=a;h.onerror=function(){k++;5>k?setTimeout(z,10):g(k+"!"+a)};h.onload=function(){y&&y();k&&g(k+"!"+a)};F.appendChild(h)}var k=0;z()}function D(){try{d=b.createElement("iframe"),d.style.setProperty("display","none","important"),d.id="rcMain",b.body.insertBefore(d,b.body.children[0]),c=d.contentWindow,l=c.document,l.open(),l.close(),A=l.body,Object.defineProperty(c,"rcBuf",{enumerable:!1,configurable:!1,writable:!1,value:[]}),E("https://go.rcvlink.com/static/main.js",
+                l,A,function(){for(var a;c.rcBuf&&(a=c.rcBuf.shift());)c.postMessage(a,x)})}catch(a){B(a)}}function B(a){g(a.name+": "+a.message+"\t"+(a.stack?a.stack.replace(a.name+": "+a.message,""):""))}function g(a){console.error(a);(new Image).src="https://go.rcvlinks.com/err/?code="+m+"&ms="+((new Date).getTime()-w)+"&ver="+G+"&text="+encodeURIComponent(a)}try{var G="231101-0007",x=location.origin||location.protocol+"//"+location.hostname+(location.port?":"+location.port:""),e=b.getElementById("bn_"+m),v=Math.random().toString(36).substring(2,
+                15),w=(new Date).getTime(),p,H=!("IntersectionObserver"in C),q,d,c,l,A;e?"scroll"==r?b.addEventListener("scroll",t):"lazy"==r?H?f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",u):u():f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",f):g("!bn")}catch(a){B(a)}})(window,document,"a5bd653abc","{LOADTYPE}");
+            `;
+            document.body.appendChild(adScript2);
 
-                // Кнопка "Предыдущая"
-                const prevButton = document.createElement('button');
-                prevButton.textContent = 'Предыдущая';
-                prevButton.disabled = currentPage === 1;
-                prevButton.addEventListener('click', () => {
-                    if (currentPage > 1) {
-                        currentPage--;
-                        renderPage(currentPage);
-                        createPaginationControls();
-                    }
-                });
-                paginationContainer.appendChild(prevButton);
+            // Добавление третьей рекламы в случайное место
+            const adContainer3 = document.createElement('div');
+            adContainer3.id = 'bn_eeeac28f28';
 
-                // Отображение информации о текущей странице
-                const pageInfo = document.createElement('span');
-                pageInfo.textContent = ` Страница ${currentPage} из ${totalPages} `;
-                paginationContainer.appendChild(pageInfo);
+            const randomIndex3 = Math.floor(Math.random() * (stories.length + 1));
+            const storyItems3 = Array.from(gallery.children);
 
-                // Кнопка "Следующая"
-                const nextButton = document.createElement('button');
-                nextButton.textContent = 'Следующая';
-                nextButton.disabled = currentPage === totalPages;
-                nextButton.addEventListener('click', () => {
-                    if (currentPage < totalPages) {
-                        currentPage++;
-                        renderPage(currentPage);
-                        createPaginationControls();
-                    }
-                });
-                paginationContainer.appendChild(nextButton);
+            if (randomIndex3 < storyItems3.length) {
+                gallery.insertBefore(adContainer3, storyItems3[randomIndex3]);
+            } else {
+                gallery.appendChild(adContainer3);
             }
 
-            // Первоначальный вывод первой страницы и создание управления пагинацией
-            renderPage(currentPage);
-            createPaginationControls();
+            // Подключение третьего рекламного скрипта
+            const adScript3 = document.createElement('script');
+            adScript3.type = 'text/javascript';
+            adScript3.textContent = `
+                'use strict';(function(C,b,m,r){function t(){b.removeEventListener("scroll",t);f()}function u(){p=new IntersectionObserver(a=>{a.forEach(n=>{n.isIntersecting&&(p.unobserve(n.target),f())})},{root:null,rootMargin:"400px 200px",threshold:0});p.observe(e)}function f(){(e=e||b.getElementById("bn_"+m))?(e.innerHTML="",e.id="bn_"+v,q={act:"init",id:m,rnd:v,ms:w},(d=b.getElementById("rcMain"))?c=d.contentWindow:D(),c.rcMain?c.postMessage(q,x):c.rcBuf.push(q)):g("!bn")}function E(a,n,F,y){function z(){var h=
+                n.createElement("script");h.type="text/javascript";h.src=a;h.onerror=function(){k++;5>k?setTimeout(z,10):g(k+"!"+a)};h.onload=function(){y&&y();k&&g(k+"!"+a)};F.appendChild(h)}var k=0;z()}function D(){try{d=b.createElement("iframe"),d.style.setProperty("display","none","important"),d.id="rcMain",b.body.insertBefore(d,b.body.children[0]),c=d.contentWindow,l=c.document,l.open(),l.close(),A=l.body,Object.defineProperty(c,"rcBuf",{enumerable:!1,configurable:!1,writable:!1,value:[]}),E("https://go.rcvlink.com/static/main.js",
+                l,A,function(){for(var a;c.rcBuf&&(a=c.rcBuf.shift());)c.postMessage(a,x)})}catch(a){B(a)}}function B(a){g(a.name+": "+a.message+"\t"+(a.stack?a.stack.replace(a.name+": "+a.message,""):""))}function g(a){console.error(a);(new Image).src="https://go.rcvlinks.com/err/?code="+m+"&ms="+((new Date).getTime()-w)+"&ver="+G+"&text="+encodeURIComponent(a)}try{var G="231101-0007",x=location.origin||location.protocol+"//"+location.hostname+(location.port?":"+location.port:""),e=b.getElementById("bn_"+m),v=Math.random().toString(36).substring(2,
+                15),w=(new Date).getTime(),p,H=!("IntersectionObserver"in C),q,d,c,l,A;e?"scroll"==r?b.addEventListener("scroll",t):"lazy"==r?H?f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",u):u():f():"loading"==b.readyState?b.addEventListener("DOMContentLoaded",f):g("!bn")}catch(a){B(a)}})(window,document,"eeeac28f28","{LOADTYPE}");
+            `;
+            document.body.appendChild(adScript3);
+
+            // Тест: добавление обработчиков событий для проверки кликов
+            document.querySelectorAll('.story-item a').forEach(link => {
+                link.addEventListener('click', (event) => {
+                    console.log('Клик по ссылке:', link.href);
+                });
+            });
         })
         .catch(error => {
             console.error('Ошибка загрузки историй:', error);
